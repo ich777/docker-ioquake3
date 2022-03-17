7747ce912265687fab224daea8f6f9ce735e7c43
ich777 <christophhummer@gmail.com>
2022-03-17 12:57:15 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,2 +1,59 @@
 #!/bin/bash
-sleep infinity
+
+echo "---Checking if ioquake3 is installed---"
+if [ ! -f ${DATA_DIR}/ioquake3 ]; then
+  echo "---Quake 3 not found, downloading...---"
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/ioquake3.zip "${DL_URL}" ; then
+    echo "---Successfully downloaded ioquake3---"
+  else
+    echo "---Can't download ioquake3 please check your download URL, putting server into sleep mode---"
+    sleep infinity
+  fi
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/ioquakepatch.zip "${DL_URL_PATCH}" ; then
+    echo "---Successfully downloaded ioquake3 Patch---"
+  else
+    echo "---Can't download ioquake3 Patch please check your download URL, putting server into sleep mode---"
+    sleep infinity
+  fi
+  mkdir -p ${DATA_DIR}/ioquake3ded ${DATA_DIR}/.q3a/baseq3
+  unzip -d ${DATA_DIR}/ioquake3ded ${DATA_DIR}/ioquake3.zip
+  unzip -d ${DATA_DIR}/ioquake3ded ${DATA_DIR}/ioquake3ded/*.zip
+  unzip -d ${DATA_DIR} ${DATA_DIR}/ioquakepatch.zip
+  cp -R ${DATA_DIR}/*-pk3s/baseq3/* ${DATA_DIR}/ioquake3ded/baseq3/
+  cp -R ${DATA_DIR}/*-pk3s/missionpack/* ${DATA_DIR}/ioquake3ded/missionpack/
+  rm -rf ${DATA_DIR}/ioquake3.zip $(ls -f ${DATA_DIR}/ioquake3ded/*.zip 2>/dev/null) ${DATA_DIR}/ioquakepatch.zip $(ls -d ${DATA_DIR}/*-pk3s 2>/dev/null) 2>/dev/null
+else
+	echo "---ioquake3 found, continuing...---"
+fi
+
+echo "---Checking if .pk3 files are present---"
+if [ ! -f ${DATA_DIR/.q3a/baseq3/pak0.pk3 ]; then
+  echo "-----------------------------------------------------------"
+  echo "---No pak file found in your .../.q3a/baseq3/ folder...----"
+  echo "----Please paste all your pak*.pk3 files from your game----"
+  echo "------directory into your .../.q3a/baseq3/ folder and -----"
+  echo "---restart the container, putting server into sleep mode---"
+  echo "-----------------------------------------------------------"
+  chmod 770 -R ${DATA_DIR}/.q3a
+  sleep infinity
+else
+  echo "---pak0.pk3 found, continuing---"
+fi
+
+echo "---Prepare Server---"
+echo "---Checking for old logs---"
+find ${DATA_DIR} -name "masterLog.*" -exec rm -f {} \;
+screen -wipe 2&>/dev/null
+
+echo "---Server ready---"
+chmod -R ${DATA_PERM} ${DATA_DIR}
+
+echo "---Starting server---"
+cd ${DATA_DIR}
+screen -S ioquake3 -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${DATA_DIR}/ioquake3ded/ioq3ded.x86_64 +set net_ip 0.0.0.0 +set net_port ${IOQ3_PORT} +map ${Q3_MAP} ${GAME_PARAMS}
+sleep 2
+if [ "${ENABLE_WEBCONSOLE}" == "true" ]; then
+  /opt/scripts/start-gotty.sh 2>/dev/null &
+fi
+screen -S watchdog -d -m /opt/scripts/start-watchdog.sh
+tail -f ${SERVER_DIR}/masterLog.0
